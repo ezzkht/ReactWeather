a62f80d6de04247570152009fec1a3908f57682b
Ezz Khayyat <ezz_kht@hotmail.com>
2017-08-22 22:52:14 +0300

    Update about page copy

--- public/bundle.js
+++ public/bundle.js
@@ -26618,9 +26618,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About!'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to about page!'
+	    )
 	  );
 	};
 
